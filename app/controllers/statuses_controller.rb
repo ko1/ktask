713d5ed1c2c4ed3c00ebb6e1a29f4e342b4fb1ed
Koichi Sasada <ko1@atdot.net>
2016-10-07 07:55:35 +0900

    more pretty

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -3,6 +3,7 @@ class StatusesController < ApplicationController
   # GET /statuses.json
   def index
     @tasks = Task.all
-    @results = Result.all
+    @completed_results = Result.where.not(completed_at: nil)
+    @not_completed_results    = Result.where(completed_at: nil)
   end
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -24,6 +24,39 @@ class Result < ApplicationRecord
 
   def status_with_time
     st = _status
-    "#{st[0]} at #{st[1]}"
+    "#{st[0]} at #{time_rep(Integer(Time.now - st[1]))} ago"
+  end
+
+  def worker_signature
+    if /\A(.+?)-/ =~ w = self.worker
+      $1
+    else
+      w
+    end
+  end
+
+  def status_with_length
+    s, t = _status
+    if s == 'completed'
+      "#{s} (#{time_rep Integer(t - self.invoked_at)})"
+    else
+      s
+    end 
+  end
+
+  def time_rep sec
+    case 
+    when sec > 60 * 60
+      h = sec / (60 * 60)
+      m = (sec % (60 * 60)) / 60
+      s = sec % 60
+      "#{h}h#{m}m#{s}s"
+    when sec > 60
+      m = sec / 60
+      s = sec % 60
+      "#{m}m#{s}s"
+    else
+      "#{sec}s"
+    end
   end
 end
